43678993f3c58396bea362fd3c2691a57cbf82a9
Shashank Mallipeddi <98105669+shashmalli@users.noreply.github.com>
2025-10-01 20:40:18 -0400

    back to min req

--- Journal.py
+++ Journal.py
@@ -1,3 +1,5 @@
+import csv
+import json
 from JournalEntry import JournalEntry
 
 class Journal:
@@ -8,6 +10,25 @@ class Journal:
     entry = JournalEntry(text, date)
     with open(self.filename, "a") as f:
       f.write(f"\n>{entry.date}\n{entry.entry}\n")
+  
+  def write_from_file(self, file):
+    try:
+      if file.lower().endswith(".csv"):
+        with open(file, newline="") as f:
+          lines = list(csv.DictReader(f))
+      elif file.lower().endswith(".json"):
+        with open(file, "r") as f:
+          lines = json.load(f)
+      else:
+        raise ValueError
+    except ValueError:
+      print("Warning: Unsupported file type (must be .csv or .json)\n")
+    except FileNotFoundError:
+      print("Warning: File does not exist\n")
+    else:
+      with open(self.filename, "a") as j:
+        for line in lines:
+          j.write(f"\n>{line['date']}\n{line['entry']}\n")
 
   def read_entries(self):
     with open(self.filename, "r") as f:
--- project0.py
+++ project0.py
@@ -1,6 +1,18 @@
 from datetime import date
 from Journal import Journal
 
+def write_to_journal(journal):
+  w_method = input("Import file (I) or Enter Manually (W)\n")
+  if w_method.upper() == "W":
+    journal_entry = input("Start your entry below:\n")
+    journal.add_entry(journal_entry, date.today())
+  elif w_method.upper() == "I":
+    user_file = input("Enter the name of the file: ")
+    journal.write_from_file(user_file)
+  else:
+    print("You must enter a letter for one of the options\n")
+    return
+
 def main():
   myJournal = Journal("journal.txt")
 
@@ -9,10 +21,12 @@ def main():
     if usr_in.upper() == "R":
       myJournal.read_entries()
     elif usr_in.upper() == "W":
-      journal_entry = input("Start your entry below:\n")
-      myJournal.add_entry(journal_entry, date.today())
+      write_to_journal(myJournal)
     elif usr_in.upper() == "Q":
       break
+    else:
+      print("You must enter a letter for one of the options")
+
 
 if __name__ == "__main__":
   main()
